980362db9cecddeab20f658c16b51e09fc1dcb92
Aidar <ajdar.mar@yandex.ru>
2024-09-26 18:58:57 +0300

    Made backend

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Localization
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var langCode = Localization.Properties.Settings.Default.langCode;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(langCode);
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Localization.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Localization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new MainPage());
+        }
+
+        private void Test_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Test.SelectedIndex == 0)
+                Properties.Settings.Default.langCode = "en-US";
+            else if (Test.SelectedIndex == 1)
+                Properties.Settings.Default.langCode = "ru-RU";
+            Properties.Settings.Default.Save();
+        }
+    }
+}
